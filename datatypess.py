6ed2049d1cb4f6c97bcbcf21cce051a2c53c4882
androthuswathi <swathiandrothu@gmail.com>
2025-06-26 15:45:03 +0530

    Add files via upload

---
+++ datatypess.py
@@ -0,0 +1,51 @@
+#int
+a=15
+print(a)
+print(type(a)) 
+print(id(a))
+
+#float
+a= 15.28
+print(a)
+print(type(a))
+print(id(a))
+
+#complex
+a= 5+2j
+print(a)
+print(type(a))
+print(id(a))
+
+#string
+a="ramcharan"
+print(a)
+print(type(a))
+print(id(a))
+
+#list
+mylist = [28,28.15,29+15j,"teju"]
+print(mylist)
+print(type(mylist))
+print(id(mylist))
+
+#tuple
+mytuple = (28,28.15,29+15j,"teju")
+print(mytuple)
+print(type(mytuple))
+print(id(mytuple))
+
+#bool
+a= True
+print(a)
+print(type(a))
+
+#set
+myset={26, 26.5, 23+5j, "sri"}
+print(myset)
+print(type(myset))
+print(id(myset))
+
+#dictionary
+mydic={"name":'tejasri'}
+print(mydic)
+print(type(mydic))
